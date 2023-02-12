be826185860cac4d1f8d270c20b6fc101a6c08a6
Mordzio <dszewczyk11@gmail.com>
2023-02-12 02:40:48 +0100

    added message when greeting/removing members

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 import responses
 
 
@@ -15,10 +16,12 @@ async def send_message(message, user_message, is_private):
 
 def run_discord_bot():
     token = "MTA1MDAyOTQyODQ2MzM3ODQ5Mw.GGQQkr.DR8qlfCrhCJkADH8lUknccJAv9RY18eVkWONy8"
+    BOT_FLAG = '!'
 
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
+    intents.members = True
+    client = commands.Bot(command_prefix='!', intents=intents)
 
     @client.event
     async def on_ready():
@@ -34,10 +37,28 @@ def run_discord_bot():
         channel = str(message.channel)
 
         print(f"{username} said {user_message} in channel {channel}")
-        if user_message[0] == '?':
-            await send_message(message, user_message[1:],  is_private=True)
-        else:
-            await send_message(message, user_message[1:], is_private=False)
+        try:
+            if user_message[0] == '?':
+                await send_message(message, user_message[1:],  is_private=True)
+            elif user_message[0] == BOT_FLAG:
+                await send_message(message, user_message[1:], is_private=False)
+                print("bot falg working")
+            else:
+                pass
+        except Exception as e:
+            print(e)
+
+    @client.event
+    async def on_member_join(member):
+        channel_id = 1074133871962099732
+        channel = client.get_channel(channel_id)
+        await channel.send(f'{member} has now officially become a loser')
+
+    @client.event
+    async def on_member_remove(member):
+        channel_id = 1074133871962099732
+        channel = client.get_channel(channel_id)
+        await channel.send(f"{member} has regained it's connection the outer world")
 
 
     client.run(token)
