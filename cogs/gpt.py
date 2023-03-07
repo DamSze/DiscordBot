68a6b4b5a863c3b4764f71b693244160994c66b9
Mordzio <dszewczyk11@gmail.com>
2023-03-07 19:25:13 +0100

    minor upgrades

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -4,12 +4,14 @@ import requests
 import io
 from discord.ext import commands
 from const.constants import OPEN_AI_KEY
+from const.constants import EMOJI
 
 
 class Gpt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.key = OPEN_AI_KEY
+        self.error_emoji = EMOJI['actually']
         openai.api_key = self.key
 
     @commands.command()
@@ -31,6 +33,9 @@ class Gpt(commands.Cog):
     @commands.command(aliases=['image', 'Aiimage', 'imageAi', 'picture'])
     async def gptimage(self, ctx, *args):
         prompt = " ".join(args)
+        embed = discord.Embed(description="WAIT FOR YOUR IMAGES",
+                              color=discord.Color.green())
+        await ctx.send(embed=embed)
         images = openai.Image.create(
             prompt=prompt,
             n=4,
@@ -39,10 +44,21 @@ class Gpt(commands.Cog):
         test = images['data']
         for img in test:
             file = self.downloadImage(img['url'])
+            if not file:
+                commands.CommandError("can't download image")
+                return
             await ctx.channel.send(file=discord.File(file, 'image.png'))
+
+    @gptimage.error
+    async def gptimage_error(self, ctx, error):
+        embed = discord.Embed(description=f'{self.error_emoji} {error} {self.error_emoji}',
+                              color=discord.Color.red())
+        await ctx.send(embed=embed)
 
     def downloadImage(self, url):
         img = requests.get(url)
+        if img.status_code != 200:
+            return False
         img_content = img.content
         return io.BytesIO(img_content)
 
